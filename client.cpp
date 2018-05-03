e254a8b19ff4abec35e54abbae35251bc669a6ad
spsnk <salvador.paz.santos@gmail.com>
2018-05-02 23:01:52 -0500

    client working, small bug on route calculation

--- client.cpp
+++ client.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <vector>
 #include <unistd.h>
+#include <cmath>
 #include "PaqueteDatagrama.h"
 #include "SocketDatagrama.h"
 #include "data.h"
@@ -13,44 +14,70 @@ main (int argc, char* argv[])
 {
   if (argc < 3)
   {
-    cout << "Missing argument. Usage: client ip port";
+    cout << "Missing argument. Usage: client ip port" << endl;
     return 1;
   }
-  char *ip;
-  memcpy( ip, argv[1], strlen(argv[1]) );
+  char ip[16];
+  memcpy( ip, argv[1], strlen(argv[1])+1 );
   int port = atoi(argv [2]);
   data msj;
   msj.id = -1;
-  SocketDatagrama client (0);
+  msj.valid = true;
+  SocketDatagrama  client (0);
   PaqueteDatagrama pk_send (&msj, sizeof (msj), ip, port);
-  
-  while (1)
+  float x_0 = -1;
+  float y_0 = -1;
+  cout << "Sending initialization packet to " << ip << ":" << port << endl;
+  while ( msj.valid )
   {
-    cout << "->" << ip << ":" << port << endl;
+    cout << "->" << endl;
     msj.print();
     PaqueteDatagrama pk_send (&msj, sizeof (msj), ip, port);
     client.envia(pk_send);
     
     cout << "Waiting response..." << endl;
-    
+    PaqueteDatagrama pk_recv (sizeof (msj));
     client.recibe(pk_recv);
     msj = *pk_recv.obtieneDatos();
+    cout << "<-" << endl;
     msj.print();
+    
+    if( x_0 < 0 || y_0 < 0 )
+    {
+      x_0 = msj.x;
+      y_0 = msj.y;
+    }
     
     float nx = msj.x;
     float ny = msj.y;
-    float dx = msj.x_dest;
-    float dy = msj.y_dest;
+    float dx = msj.x_dest - nx;
+    float dy = msj.y_dest - ny;
     float sp = msj.speed;
+    float mag = sqrt( dx*dx + dy*dy );
     
-    if (valid)
+    dx /= mag;
+    dy /= mag;
+    dx *= sp;
+    dy *= sp;
+    nx += dx;
+    ny += dy;
+    
+    msj.x = nx;
+    msj.y = ny;
+    
+    if ( 
+        ( (msj.x <= ( (int) msj.x_dest + 5) ) &&
+          (msj.x >= ( (int) msj.x_dest - 5) ) ) &&
+        ( (msj.y <= ( (int) msj.y_dest + 5) ) &&
+          (msj.y >= ( (int) msj.y_dest - 5) ) )
+       )
     {
-      cout << "El numero es primo" << endl;
-    }
-    else
-    {
-      cout << "El numero NO es primo" << endl;
-    }
-    usleep(16666)
+      msj.valid = false;
+    }  
+    usleep(16666);
   }
+  
+  cout << "Route completed\nDistance traveled: " << (msj.x - x_0)/(msj.y - y_0) << " pixels" << endl;
+  
+  return 0;
 }
